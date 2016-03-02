b7e7eee85c0f5764c91603ab260b59122ed62aee
Jeffrey Yan <yanj@usc.edu>
2016-03-02 02:24:31 -0800

    adding class php files

---
+++ Server/Frontend/buySellStock.php
@@ -0,0 +1,62 @@
+<html>
+<body>
+	<?php
+
+		include_once('../Portfolio.class.php');
+		include_once('../Account.class.php');
+		include_once('../Stock.class.php');
+		include_once('../TrackedStock.class.php');
+		include_once('../OwnedStock.class.php');
+		include_once('YahooFinance.php');
+
+		//Enable global variables
+	 	session_start();
+
+	 	//Allow for errors to be displayed
+		ini_set('display_errors', 1);
+		ini_set('display_startup_errors', 1);
+		error_reporting(E_ALL);
+	
+		//Initialize parse and the classes it needs
+		require '../../vendor/autoload.php';
+		use Parse\ParseUser;
+		use Parse\ParseClient;
+		use Parse\ParseException;
+		ParseClient::initialize('YtTIOIVkgKimi9f3KgvmhAm9be09KaFPD0lK1r21', 'Bxf6gl3FUT0goWvvx3DIger9bcOjwY1LflXr6MIO', 'r86cSKPWagMCavzJXVF4OFnte5yPpNY74GhY9UxS');
+
+		
+
+		if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+			if($_POST['action'] == 'Buy'){
+				$globalAccount = $_SESSION['account'];
+				echo(var_dump($globalAccount));
+				$tickerName = $_POST["ticker"];
+				$stockQuantity = $_POST["quantity"];
+				
+				$objYahooStock = new YahooStock;
+				$objYahooStock->addFormat("snl1d1t1c1p2"); 
+				$objYahooStock->addStock($tickerName);
+				$stockPrice;
+				foreach( $objYahooStock->getQuotes() as $code => $stock)
+				{
+			  		$stockPrice = floatval($stock[2]);
+				}
+
+				$globalAccount->getPortfolio()->buyStock($tickerName,$stockQuantity,$stockPrice);
+			}
+			else if($_POST['action'] == 'Sell'){
+
+				$globalAccount = $_SESSION['account'];
+				$tickerName = $_POST["ticker"];
+				$stockQuantity = $_POST["quantity"];
+
+				$globalAccount->getPortfolio()->sellStock($tickerName,$stockQuantity);
+
+			}
+		}
+
+
+	?>
+</body>
+</html>
--- Server/Stock.class.php
+++ Server/Stock.class.php
@@ -1,28 +1,59 @@
 <?php
 
-class Stock
+abstract class Stock
 {
 	public $ticker;
 	public $currentValue;
 	public $companyName;
 	public $initialDate;
-	public $historicalData = array();
 	
-	public function __construct($ticker, $initialDate)
-	{
-		$this->ticker = $ticker;
-		$this->initialDate = $initialDate;
-	}
+	
 	
 	public function addHistoricalData($price, $date)
 	{
-		$this->historicalData[$date] = array("price"=>$price, "date"=>$date);
+		$this->historicalData[] = array("price"=>$price, "date"=>$date);
 	}
 	
 	public function outputData()
 	{
 		var_dump($this->historicalData);
 	}
+
+	public function getTicker(){
+		return (string)$ticker;
+	}
+
+	public function setTicker($ticker){
+		$this->ticker = $ticker;
+	}
+
+	public function getCurrentValue(){
+		return $currentValue;
+	}
+
+	public function setCurrentValue($currentValue){
+		$this->currentValue = $currentValue;
+	}
+
+	public function getCompanyName(){
+		return $companyName;
+	}
+
+	public function setCompanyName($companyName){
+		$this->companyName = $companyName;
+	}
+
+	public function getInitialDate(){
+		return $initialDate;
+	}
+
+	public function setInitialDate($initialDate){
+		$this->initialDate = $initialDate;
+	}
+
+	
+
+
 }
 
 ?>
