6e0081cc7bb14ca26c5fcd61c4d150152833e938
Jonathan Qin <jonzqin@gmail.com>
2016-03-02 02:42:54 -0800

    styled lists, added functionality to searchbar and remove button in watchlist (they update the db)

--- Server/Frontend/retrieveWatchlistYahoo.php
+++ Server/Frontend/retrieveWatchlistYahoo.php
@@ -15,20 +15,42 @@
 		$objYahooStock = new YahooStock;
 		$objYahooStock->addFormat("snl1d1c1p2"); 
 
-		$stockNames = array("AAPL", "GOOG", "AMZN", "FB");//hard coded test case for now
+		$stockNames = array("AAPL", "CMG", "COST", "FB", "GOOG");//hard coded test case for now
 		//*****UPDATE LOCAL VARIABLES*****$stockNames = _SESSION["ownedStocks"];
 		foreach($stockNames as $code => $stock){
 			$objYahooStock->addStock((string)$stock);
 		}
 
 		foreach( $objYahooStock->getQuotes() as $code => $stock){
+
 			?>
-			<?php echo $stock[0]; ?> <?php echo $stock[1]; ?> <br />
-			Last Trade Price: <?php echo'$ '.$stock[2]; ?> <br />
-			Last Trade Date: <?php echo $stock[3]; ?> <br />
-			Dollar Change: <?php echo '$ '. $stock[4]; ?> <br />
-			Percent Change: <?php echo $stock[5]; ?> <br /><br />
+			<!doctype html>
+
+			<html>
+
+			<link rel="stylesheet" type="text/css" href="style.css">
+			
+			<div class="stockmodule" id=<?php echo $stock[0]; ?> >
+
+
+				<b><?php echo $stock[1]; ?> (<?php echo $stock[0]; ?>)</b> <br />
+				<b>Last Trade Price:</b> <?php echo'$ '.$stock[2]; ?> <br />
+				<b>Last Trade Date:</b> <?php echo $stock[3]; ?> <br />
+				<b>Dollar Change:</b> <?php echo '$ '. $stock[4]; ?> <br />
+				<b>Percent Change:</b> <?php echo $stock[5]; ?> <br />
+				<input type="checkbox">Graph
+				
+				<form action="http://localhost/Frontend/removeWatchedStock.php" method="post" enctype="multipart/form-data">
+					<input hidden type="text" name="ticker" value = <?php echo $stock[0]; ?>>
+					<input type="submit" value="X">
+				</form>
+
+			</div>
+
+			</html>
+
 			<?php
+
 		}	
 	}
 
