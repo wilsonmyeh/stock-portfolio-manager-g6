84582220a8d8409b3e0eae7784138f5c4d7d205b
Wilson Yeh <wilsonye@usc.edu>
2016-03-02 04:35:09 -0800

    comments, destroy php session on logout

--- Server/Frontend/logout.php
+++ Server/Frontend/logout.php
@@ -1,4 +1,5 @@
 <?php
+	# Display Errors
 	ini_set('display_errors', 1);
 	ini_set('display_startup_errors', 1);
 	error_reporting(E_ALL);
@@ -10,9 +11,12 @@
 
 	ParseClient::initialize('YtTIOIVkgKimi9f3KgvmhAm9be09KaFPD0lK1r21', 'Bxf6gl3FUT0goWvvx3DIger9bcOjwY1LflXr6MIO', 'r86cSKPWagMCavzJXVF4OFnte5yPpNY74GhY9UxS');
 
+	# try to log user out, ending the parse and php sessions
 	try
 	{
 	 	ParseUser::logOut();
+	 	session_unset();     // unset $_SESSION variable for the run-time 
+    	session_destroy();   // destroy session data in storage
 		$message = "Successful log out!";
 		echo "<script type='text/javascript'>alert('$message');</script>";
 		readfile("http://localhost/Frontend/login.html");
