cddca1c7922b5dc50ee754fb8be78b3784029341
Wilson Yeh <wilsonye@usc.edu>
2016-03-02 01:32:55 -0800

    PHP Header CORS

--- Server/Frontend/retrieveWatchlistYahoo.php
+++ Server/Frontend/retrieveWatchlistYahoo.php
@@ -10,6 +10,8 @@
 	ini_set('display_errors', 1);
 	ini_set('display_startup_errors', 1);
 	error_reporting(E_ALL);
+	
+ 	header("Access-Control-Allow-Origin: *");
 
 	function pullWatchlistDataFromYahoo(){
 		$objYahooStock = new YahooStock;
