223bd0ffda73819b596e1fcf66cf1163da2b6bfd
Wilson Yeh <wilsonye@usc.edu>
2016-03-21 16:40:40 -0700

    backup, resize skeletons, script to run tests

--- features/step_definitions/autocomplete_step.rb
+++ features/step_definitions/autocomplete_step.rb
@@ -1,7 +1,7 @@
 When(/^user types "([^"]*)" into the searchbar$/) do |arg1|
-	
+  fill_in('searchbar', :with => username)
 end
 
 Then(/^user should see a dropdown of search results$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+	
 end
