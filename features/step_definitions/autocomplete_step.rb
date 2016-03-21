5ee90eafa016bbfae75734c46aa77f79b9be3d7b
Wilson Yeh <wilsonye@usc.edu>
2016-03-21 13:00:39 -0700

    autocomplete test

--- features/step_definitions/autocomplete_step.rb
+++ features/step_definitions/autocomplete_step.rb
@@ -1,7 +1,7 @@
 When(/^user types "([^"]*)" into the searchbar$/) do |arg1|
-  fill_in('searchbar', :with => username)
+  fill_in('searchbar', :with => arg1)
 end
 
 Then(/^user should see a dropdown of search results$/) do
-	
+	result = true;
 end
